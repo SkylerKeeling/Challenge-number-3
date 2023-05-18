63110a3a0caab070feab58f2480e5eb8215ed369
skyler <skyler@skylers-MacBook-Pro.local>
2023-05-18 13:58:19 -0300

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,59 +3,96 @@
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-var length = document.getElementById("#length");
-var lowercase = document.getElementById("#lowercase");
-var uppercase =document.getElementById("#uppercase");
-var numeric = document.getElementById("#numeric");
-var special = document.getElementById("#special");
 
-var lowercaseCodes = charCodeAt(97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120,121,122);
-var uppercaseCodes = charCodeAt(65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90);
-var numericCodes = charCodeAt(48,49,50,51,52,53,54,55,56,57);
-var specialCodes = charCodeAt(33,34,35,36,37,38,39,40,41,42,43,44,45,46,47);
+const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+const alphabetUpper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var specialCharacters = [
+  '@',
+  '%',
+  '+',
+  '\\',
+  '/',
+  "'",
+  '!',
+  '#',
+  '$',
+  '^',
+  '?',
+  ':',
+  ',',
+  ')',
+  '(',
+  '}',
+  '{',
+  ']',
+  '[',
+  '~',
+  '-',
+  '_',
+  '.',
+];
 
-const chosenLength = length.valued;
-const plusLowerCase = lowercase.checked;
-const plusUpperCase = uppercase.checked;
-const plusNumeric = numeric.checked;
-const plusSpecial = special.checked; 
-
-var password = writePassword(length, lowercaseCodes, uppercaseCodes, numericCodes, specialCodes);
-
-if length < 8 || length > 128 {
-  print("criteria does not meet requirements");
-}
-
-function check () {
 
 
-}
+
+
+
+
+
+
 
 // Write password to the #password input
 
-function writePassword(length, lowercaseCodes, uppercaseCodes, numericCodes, specialCodes) {
-    
-    if (lowercaseCodes) {return password.includes(lowercaseCodes);
+function generatePassword () {
+  
+  var passwordLength = prompt("Enter password length");
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert("criteria does not meet requirements");
+    return
+   }
+   var characterBank = [];
+
+  if (confirm ("Include lowercase letters?")) {
+    console.log("yes, use lowercase")
+    characterBank = characterBank.concat(alphabet);
+
   }
-    else if 
-    (uppercaseCodes) {return password.includes(uppercaseCodes);
-    }
-    else if 
-    (numericCodes) {return password.includes(numericCodes);
-    }
-    else if 
-    (specialCodes) {return password.includes(specialCodes);
-    }
+  if (confirm ("Include uppercase letters?")) {
+    console.log("yes, use uppercase");
+    characterBank = characterBank.concat(alphabetUpper);
+  }
+  if (confirm ("Include numbers?")) {
+    console.log("yes, use numbers");
+    characterBank = characterBank.concat(numericCharacters);
+  }
+  if (confirm ("Include special characters?")) {
+    console.log("yes, use special characters");
+    characterBank = characterBank.concat(specialCharacters);
+  }
+  console.log(characterBank);
+  var newPassword = "";
 
-    for 
-
-
-
+  for (var i = 0; i < passwordLength; i++) {
+ 
+  }
+  
+  
+  
+  return newPassword;
 }
 
-if 
+
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
 
 
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword) 
+ 
