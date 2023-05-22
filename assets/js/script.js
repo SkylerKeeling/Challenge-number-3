41d0cd0a1a18f38a2240eb3713f3998d6c5b275f
skyler <skyler@skylers-MacBook-Pro.local>
2023-05-22 13:10:29 -0300

    for loop added

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,97 +2,50 @@
 
 
 // Get references to the #generate element
+var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var alphabetUpper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var specialCharacters = ['@','%','+','\\','/',"'",'!','#','$','^','?',':',',',')','(','}','{',']','[','~','-','_','.',];
+
 var generateBtn = document.querySelector("#generate");
 
-const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-const alphabetUpper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-var specialCharacters = [
-  '@',
-  '%',
-  '+',
-  '\\',
-  '/',
-  "'",
-  '!',
-  '#',
-  '$',
-  '^',
-  '?',
-  ':',
-  ',',
-  ')',
-  '(',
-  '}',
-  '{',
-  ']',
-  '[',
-  '~',
-  '-',
-  '_',
-  '.',
-];
-
-
-
-
-
-
-
-
+generateBtn.addEventListener("click", writePassword); 
 
 
 // Write password to the #password input
 
 function generatePassword () {
-  
-  var passwordLength = prompt("Enter password length");
+  var characterBank = [];
+  var newPassword = "";
+  var passwordLength = parseInt(prompt("Enter password length"));
   if (passwordLength < 8 || passwordLength > 128) {
     alert("criteria does not meet requirements");
-    return
    }
-   var characterBank = [];
-
   if (confirm ("Include lowercase letters?")) {
-    console.log("yes, use lowercase")
     characterBank = characterBank.concat(alphabet);
-
   }
   if (confirm ("Include uppercase letters?")) {
-    console.log("yes, use uppercase");
     characterBank = characterBank.concat(alphabetUpper);
   }
   if (confirm ("Include numbers?")) {
-    console.log("yes, use numbers");
     characterBank = characterBank.concat(numericCharacters);
   }
   if (confirm ("Include special characters?")) {
-    console.log("yes, use special characters");
     characterBank = characterBank.concat(specialCharacters);
   }
-  console.log(characterBank);
-  var newPassword = "";
-
   for (var i = 0; i < passwordLength; i++) {
- 
+    var randomIndex = Math.floor(Math.random() * passwordLength);
+    newPassword = newPassword + characterBank[randomIndex];
   }
-  
-  
-  
   return newPassword;
 }
-
-
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
-}
-
+  }
 
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword) 
+
+
  
