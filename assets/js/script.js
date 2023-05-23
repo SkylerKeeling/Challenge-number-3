5eee759ca6cf866723409d60c61853c855dce2c5
skyler <skyler@skylers-MacBook-Pro.local>
2023-05-23 10:28:01 -0300

    bug resolved

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var alphabetUpper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-var specialCharacters = ['@','%','+','\\','/',"'",'!','#','$','^','?',':',',',')','(','}','{',']','[','~','-','_','.',];
+var specialCharacters = ['@','%','+','\\','/',"'",'!','#','$','^','?',':',',',')','('];
 
 var generateBtn = document.querySelector("#generate");
 
@@ -20,6 +20,7 @@ function generatePassword () {
   var passwordLength = parseInt(prompt("Enter password length"));
   if (passwordLength < 8 || passwordLength > 128) {
     alert("criteria does not meet requirements");
+    return null;
    }
   if (confirm ("Include lowercase letters?")) {
     characterBank = characterBank.concat(alphabet);
@@ -33,8 +34,12 @@ function generatePassword () {
   if (confirm ("Include special characters?")) {
     characterBank = characterBank.concat(specialCharacters);
   }
+  if (characterBank.length == 0) {
+    alert("no characters selected, please select valid options");
+    return null;
+  }
   for (var i = 0; i < passwordLength; i++) {
-    var randomIndex = Math.floor(Math.random() * passwordLength);
+    var randomIndex = Math.floor(Math.random() * characterBank.length);
     newPassword = newPassword + characterBank[randomIndex];
   }
   return newPassword;
